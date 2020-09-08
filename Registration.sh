a9c8ea51badc11db58da1c78711ce72c3f6724b1
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-08 15:04:07 +0530

    User Can Enter Last Name

--- Registration.sh
+++ Registration.sh
@@ -2,12 +2,20 @@
 
 	read -p "Enter First Name : " First_Name
 
+	read -p "Enter Last Name : " Last_Name
+
 	Pattern="^[[:upper:]]{1}[a-zA-Z0-9]{2,}$"
 
 	if [[ $First_Name =~ $Pattern ]]
 	then
-		echo Valid
+		echo First Name is Valid
 	else
-		echo Invalid
+		echo First Name is Invalid
 	fi
 
+	if [[ $Last_Name =~ $Pattern ]]
+   then
+      echo Last Name is Valid
+   else
+      echo Last Name is Invalid
+   fi
