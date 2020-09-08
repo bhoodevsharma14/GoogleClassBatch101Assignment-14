2e3038a4ad333e1470f71087e071fbd96898dd69
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-08 18:30:58 +0530

    User can Enter email Id

--- Registration.sh
+++ Registration.sh
@@ -4,18 +4,29 @@
 
 	read -p "Enter Last Name : " Last_Name
 
+	read -p "Enter User Id : " email
+
 	Pattern="^[[:upper:]]{1}[a-zA-Z0-9]{2,}$"
+
+	emailPattern="^[[:lower:]]{3}[a-zA-Z0-9]*([-+_.]?[a-zA-Z0-9])*@[a-z]{2,}.[a-z]{2,3}([.][a-z]{2})*$"
 
 	if [[ $First_Name =~ $Pattern ]]
 	then
-		echo First Name is Valid
+		echo "First Name is Valid"
 	else
-		echo First Name is Invalid
+		echo "First Name is Invalid"
 	fi
 
 	if [[ $Last_Name =~ $Pattern ]]
    then
-      echo Last Name is Valid
+      echo "Last Name is Valid"
    else
-      echo Last Name is Invalid
+      echo "Last Name is Invalid"
    fi
+
+	if [[ $email =~ $emailPattern ]]
+	then
+		echo "It's Valid Email"
+	else
+		echo "It's Invalid Email"
+	fi
