fae25654453057574d041b38056118e927bc3725
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-08 14:57:22 +0530

    User Can Enter First Name

--- Registration.sh
+++ Registration.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 
+	read -p "Enter First Name : " First_Name
+
+	Pattern="^[[:upper:]]{1}[a-zA-Z0-9]{2,}$"
+
+	if [[ $First_Name =~ $Pattern ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+
